8d5175283dd11f211b07d62949c1d5bb980a56c2
pompopo <pompopo@gmail.com>
2015-08-27 21:57:41 +0900

    add redux-logger

--- app/components/main.js
+++ app/components/main.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/index.js'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 
-
-let store = applyMiddleware(thunk)(createStore)(rootReducer);
+let logger = createLogger();
+let store = applyMiddleware(thunk, logger)(createStore)(rootReducer);
 React.render(
   <Provider store={store}>
     {() => <Root />}
